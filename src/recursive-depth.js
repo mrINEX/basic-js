9db1256a5fd79f723e2cfa2c365810f951003881
mrinex <mrinex@mail.ru>
2020-02-25 10:19:02 +0300

    feat: add task 2

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,17 +1,29 @@
 module.exports = class DepthCalculator {
-    count = 1; // Array.isArray(arr)
     calculateDepth(arr) {
-        if(arr.length === 0) {
+        let result = arr.filter((n) => Array.isArray(n));
+        let r;
+        const array = [];
+        console.log('*', arr, '**', result, '***',r);
+        if(result.length === 0) {
             return 1;
-        }
-        for(let i = 0; i < arr.length; i += 1) {
-            if(i === arr.length - 1 && !Array.isArray(arr[i])) {
-                return 1;
+        } else {
+            if(result.length > 1) {
+                
+                for(let i = 0; i < result.length; i += 1) {
+                    console.log('---before--',r);
+                    r = this.calculateDepth([result[i]]);
+                    array.push(r);
+                    console.log('---after--',r, 'array', array);
+                    if(i === result.length - 1) {
+                        return Math.max(...array);
+                    }
+                }
+                
             }
-            if(Array.isArray(arr[i])) {
-                console.log(arr[i]);
-                return this.count + this.calculateDepth(arr[i]);
-            }
+            console.log('result before:', result);
+            let sum = 1 + this.calculateDepth(result[0]);
+            console.log('result[0]:', result[0], 'sum:', sum, '-----',r);
+            return sum;
         }
     }
 };
